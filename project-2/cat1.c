f3e9a10b432562e5bfb66d5e0b9ede2bdc27357a
Cliff Kelley <ZhangXector@gmail.com>
2012-09-18 12:45:40 -0600

    Adding project 2

---
+++ project-2/cat1.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define LINELEN 512
+
+void display_lines(FILE* fp, int type);
+
+int main(int argc, char* argv[])
+{
+	FILE* fp;
+	int cmpResult;
+	int type = 0;
+
+	cmpResult = (argv[1][0] - '-');
+	if(cmpResult == 0)
+	{
+		cmpResult = strcmp(argv[1], "-n");
+		if(cmpResult != 0)
+		{
+			printf("%s is not a recognized command. Usage: ./cat1 [-n] filename.\n", argv[1]);
+			return 1;
+		}
+	}
+
+	if(argc == 1)
+		display_lines(stdin, type);
+	else if(cmpResult == 0)
+	{
+		type = 2;
+		int i;
+
+		for(i = 2; i < argc; ++i)
+		{
+			fp = fopen(argv[i], "r");
+			if (fp != NULL)
+			{
+				display_lines(fp, type);
+				fclose(fp);
+			}
+			else
+			{
+				printf("Error opening file.\n");
+				return 1;
+			}
+		}
+		return 0;
+	}
+	else
+	{
+		type = 1;
+		int i;
+
+		for(i = 1; i < argc; ++i)
+		{
+			fp = fopen(argv[i], "r");
+			if (fp != NULL)
+			{
+				display_lines(fp, type);
+				fclose(fp);
+			}
+			else
+			{
+				printf("Error opening file.\n");
+				return 1;
+			}
+		}
+		return 0;
+	}
+	printf("\n");
+}
+
+void display_lines(FILE* fp, int type)
+{
+	char line[LINELEN];
+	int num_of_lines = 0;
+	int lineNum = 1;
+
+	if(!fgets(line, LINELEN, fp))
+	{
+		printf("Error reading line from file.");
+		exit(1);
+	}
+	else while(fgets(line, LINELEN, fp))
+	{
+		if(type == 1 || type == 0)
+		{
+			if(fputs(line, stdout) == EOF)
+				exit(0);
+			num_of_lines++;
+		}
+		else
+		{
+			printf("    %i ", lineNum);
+			if(fputs(line, stdout) == EOF)
+				exit(0);
+			num_of_lines++;
+			lineNum++;
+		}
+	}
+}
