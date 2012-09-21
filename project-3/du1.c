a0d52b0da4142647d1d91d4778e6cfb5aa14eab7
ZhangXector <ZhangXector@gmail.com>
2012-09-21 16:12:35 -0600

    Testing dirent pointer name printing, works so far.

--- project-3/du1.c
+++ project-3/du1.c
@@ -1,3 +1,41 @@
+#include <stdio.h>
 #include <dirent.h>
-#include <sys/types.h>
+#include <unistd.h> // For getcwd()
 
+void list_directories(char[]);
+
+int main (int argc, char* argv[])
+{
+	if (argc == 1)
+	{
+		return 0;
+	}
+	else
+	{
+		while (--argc)
+		{
+			printf("%s:\n", *++argv);
+			list_directories(*argv);
+		}
+	}
+}
+
+void list_directories(char dirname[])
+{
+	DIR* dir_ptr;
+	struct dirent* direntp;
+
+	if((dir_ptr = opendir(dirname)) == NULL)
+	{
+		fprintf(stderr, "du1: cannot open %s\n", dirname);
+		exit(1);
+	}
+	else
+	{
+		while ((direntp = readdir(dir_ptr)) != NULL)
+		{
+			printf("%s\n", direntp->d_name);
+		}
+		closedir(dir_ptr);
+	}
+}
