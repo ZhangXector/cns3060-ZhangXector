92f27d90931f4e96f9a97c238fa164ebcde66e84
JAcKDuB <jonwarner@outlook.com>
2012-10-04 14:30:52 -0600

    Touched up code and readied for submission.

--- project-3/README.md
+++ project-3/README.md
@@ -4,4 +4,4 @@ Project 3 - du
 Cliff Kelley
 ------------
 
-
+I found that this project was quite a bit harder than the previous projects. I started last week and was able to get the directories to list, then the sub-directories. After I figured out how to list off the files as well as the directories, I found that the sizes weren't matching up with what du -ab would report. It took a while to re-work the project to add up the sizes of the files and the sub-directories to make sure that the sizes were reporting correctly. Definite brain-buster this one was.
--- project-3/du1.c
+++ project-3/du1.c
@@ -1,24 +1,35 @@
+// Project 3 - du
+// Cliff Kelley
+
 #include <stdio.h>
 #include <dirent.h>
 #include <sys/stat.h>
-#include <sys/types.h>
-#include <string.h>
 
 #define SUCCESS 0
 #define FAILURE -1
-
+#define DIR_STRING_SIZE 256
 #define CURRENT_DIR "."
 #define PARENT_DIR ".."
 
+// List Directories
+// Params: String directory name
+// Returns: Int size of directory
+// Purpose: Reads through directory for entries and calculates size
 int list_directories(const char*);
 
+//=========================================================================================================
+
+// Main
 int main (int argc, char* argv[])
 {
+	// If there are less than or exactly 2 arguments
 	if (argc <= 2)
 	{
+		// Set the directory to the argument, or to the current dir if there isn't one
 		const char * directory = argc > 1 ? argv[1] : CURRENT_DIR;
 		struct stat dirStats;
 		
+		// Get stats on directory
 		if(stat(directory, &dirStats) != SUCCESS)
 		{
 			printf("Not a valid file or directory: %s\n", directory);
@@ -26,72 +37,105 @@ int main (int argc, char* argv[])
 		}
 		else
 		{
-			int dummy = 0;
+			// If argument is a directory
 			if(S_ISDIR(dirStats.st_mode))
 			{
-				dummy = list_directories(directory);
+				// List the directory and it's sub-directories
+				if(list_directories(directory) == -1)
+				{
+					return FAILURE;
+				}
 			}
+			// Else (it's a file name)
 			else
 			{
-				printf("%8lld B     '%s'\n", (long long)dirStats.st_size, directory);
+				// Print info on the file
+				printf("%8lld B     %s\n", (long long)dirStats.st_size, directory);
 			}
 		}
 	}
+	
+	// Else (too many arguments) 
 	else
 	{
 		printf("Too many arguments. Usage: ./du1 [directory]\n");
 		return FAILURE;
 	}
 	return SUCCESS;
-}
+}// End main
+
+//=========================================================================================================
 
 int list_directories(const char* dirname)
 {
 	DIR* directoryPointer;
 	struct dirent* currentEntry;
 	struct stat entryStats;
-	int dirSizes = 4096;
+	
+	// Get stats on the directory
+	if (stat(dirname, &entryStats) != SUCCESS)
+	{
+		perror("Failed to get directory stats\n");
+		return(FAILURE);
+	}
 
+	// Start out with the directory size
+	int dirSize = entryStats.st_size;
+
+	// Open the directory
 	if((directoryPointer = opendir(dirname)) == NULL)
 	{
 		perror(("du1: cannot open %s\n", dirname));
-		exit(-1);
+		return(FAILURE);
 	}
 	else
 	{
+		// While there are still more items in the directory
 		while ((currentEntry = readdir(directoryPointer)) != NULL)
 		{
-			char statDirectory[512];
+			// String for storing entry path (including parent directory)
+			char entryPath[DIR_STRING_SIZE];
 			
-			snprintf(statDirectory, sizeof(statDirectory) - 1, "%s/%s", dirname, currentEntry->d_name);
-			if (stat(statDirectory, &entryStats) != SUCCESS) 
+			// Create a string with the directory name and the current entry name
+			snprintf(entryPath, sizeof(entryPath) - 1, "%s/%s", dirname, currentEntry->d_name);
+			
+			// Get stats on the entry director
+			if (stat(entryPath, &entryStats) != SUCCESS) 
 			{
 				perror("Failed to get file stats\n");
 			}
+			// Check to make sure we're not looking at the parent or current directory
 			else if(strcmp(currentEntry->d_name, PARENT_DIR) && strcmp(currentEntry->d_name, CURRENT_DIR))
 			{
+				// If this entry is a directory
 				if(S_ISDIR(entryStats.st_mode))
 				{
-					char fullDir[512];
-					snprintf(fullDir, sizeof(fullDir) - 1, "%s/%s", dirname, currentEntry->d_name);
-					if(strcmp(currentEntry->d_name, CURRENT_DIR))
-					{
-						dirSizes += list_directories(fullDir);
-					}
+					// Call list_directories on this directory and add the size to dirSize
+					dirSize += list_directories(entryPath);
 				}
+				// Else (It's a file)
 				else
 				{
-					dirSizes += entryStats.st_size;
-					printf("%8lld B     '%s/%s'\n", (long long)entryStats.st_size, dirname, currentEntry->d_name, "");
+					// Add the file size to the dirSize
+					dirSize += entryStats.st_size;
+					// Print file information
+					printf("%8lld B     %s/%s\n", (long long)entryStats.st_size, dirname, currentEntry->d_name, "");
 				}
-			}
-		}
+			}// End else if
+		}// End while more items
+		
+		// Close the file
 		if(closedir(directoryPointer)!= SUCCESS)
 		{
 			perror("Failed to close the directory.");
-			exit(-1);
+			return(FAILURE);
 		}
-		printf("%8lld B     '%s'\n", (long long)dirSizes, dirname);
-		return dirSizes;
-	}
-}
+
+		// Print directory information
+		printf("%8lld B     %s\n", (long long)dirSize, dirname);
+		
+		// Return the directory size
+		return dirSize;
+
+	}// End else
+}// End list_directories
